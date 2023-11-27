bc70c7a6fd65cfd8a0be0f1587fdf0fef545951c
jordanvidal1 <jv@coinburp.com>
2023-11-27 12:35:23 +0000

    feat: create transactions csv

--- index.ts
+++ index.ts
@@ -19,12 +19,28 @@ const fetchApi = async (
     console.log('Starting script');
 
     const jsonData = await fetchApi(`https://blockchain.info/rawaddr/${address}`);
+    const transactions = jsonData.txs;
     delete jsonData.txs;
     const parser = new Parser({header: true});
 
     try {
         const csv = parser.parse(jsonData);
         const filename = `${address}.csv`;
+
+        await new Promise<void>((resolve, reject) => {
+            fs.writeFile(filename, csv, err => {
+                if(err) reject(err);
+                resolve();
+            });
+        });
+    }
+    catch(ex) {
+        console.debug(ex);
+    }
+
+    try {
+        const csv = parser.parse(transactions);
+        const filename = `${address}_transactions.csv`;
 
         await new Promise<void>((resolve, reject) => {
             fs.writeFile(filename, csv, err => {
